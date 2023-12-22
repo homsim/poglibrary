efea84333f8b9fccd12772e0874b473e547286c3
homsim <simon.homann.1997@gmail.com>
2023-12-22 11:12:29 +0100

    library object

---
+++ Backend/src/Library.h
@@ -0,0 +1,13 @@
+#include<vector>
+
+class Library
+{
+    /*
+    Holds informations about the library. Should be created upon startup
+    either from scratch (empty library) or from the database.    
+    */
+private:
+    /* data */
+public:
+    Library(/* args */);
+};
