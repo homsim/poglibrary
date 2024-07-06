8fbd33b42b7309cbb588573574860777198085c7
homsim <simon@fedora.fritz.box>
2024-07-06 15:21:16 +0200

    disabled CSRF in Spring Security

--- backend/src/main/java/com/poglibrary/backend/security/WebSecurityConfig.java
+++ backend/src/main/java/com/poglibrary/backend/security/WebSecurityConfig.java
@@ -1,16 +1,48 @@
 package com.poglibrary.backend.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)  // XSRF protection not needed for REST APIs
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
+
+/*
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -43,3 +75,4 @@ public class WebSecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
 }
+ */
